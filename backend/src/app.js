a32cdfe8502cb3e6304d649c3f1c0cc575467a1f
thallysonjsa <thallyson.alves@ccc.ufcg.edu.br>
2021-12-14 19:32:20 -0300

    adicionando pequenos detalhes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,9 @@
-const express = require('express')
-const routes = require('./routes')
-const app = express()
+const express = require('express');
+const routes = require('./routes');
+const app = express();
 
-app.use(routes)
+app.use(routes);
 
 app.listen(3000, () => {
-  console.log('Listening on port 3000!')
+  console.log('Listening on port 3000!');
 })
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,18 +3,18 @@ const filme = require('./model/filme');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-  const filmes = await filme.find({})
+  const filmes = await filme.find({});
   return res.status(200).send({
     "filmes": filmes
-  })
+  });
 });
 
 router.post('/filme', async (req, res) => {
   const { titulo, ano, duracao, diretor, sinopse, capa } = req.body;
-  const f = await filme.create({ titulo, ano, duracao, diretor, sinopse, capa })
+  const f = await filme.create({ titulo, ano, duracao, diretor, sinopse, capa });
   return res.status(201).send({
     'filme': f
-  })
+  });
 });
 
 
